0c3526e2b43a48a49630daa548ea1b11a0463d8d
Manca Bizjak <manca.bizjak@xlab.si>
2016-04-15 10:56:20 +0200

    Initial setup for https

--- server.go
+++ server.go
@@ -1,24 +1,24 @@
 package main
 
 import (
-	"github.com/xlab-si/e2ee-server/routers"
-	"github.com/xlab-si/e2ee-server/settings"
-	"github.com/xlab-si/e2ee-server/core/db"
-	"github.com/xlab-si/e2ee-server/config"
 	"github.com/codegangsta/negroni"
 	"github.com/rs/cors"
+	"github.com/xlab-si/e2ee-server/config"
+	"github.com/xlab-si/e2ee-server/core/db"
+	"github.com/xlab-si/e2ee-server/routers"
+	"github.com/xlab-si/e2ee-server/settings"
 	"net/http"
 )
 
 func main() {
 	c := cors.New(cors.Options{
-                AllowedOrigins:   []string{"*"},
-                AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE"},
-                AllowCredentials: true,
+		AllowedOrigins:   []string{"*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE"},
+		AllowCredentials: true,
 		//AllowedHeaders: []string{"Authorization"}, // doesn't work
-		AllowedHeaders:   []string{"*"},
+		AllowedHeaders: []string{"*"},
 		//Debug: true,
-        })
+	})
 
 	config.Init()
 	settings.Init()
@@ -27,5 +27,6 @@ func main() {
 	n := negroni.Classic()
 	n.Use(c)
 	n.UseHandler(router)
-	http.ListenAndServe(":8080", n)
+	//http.ListenAndServe(":8080", n)
+	http.ListenAndServeTLS(":4443", "server.crt", "server.key", n)
 }
