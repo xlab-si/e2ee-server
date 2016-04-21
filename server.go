fa7ab69a2ea2c1cc46308d7468867af79b178875
mancabizjak93@gmail.com <manca.bizjak@guest.arnes.si>
2016-04-21 18:36:52 +0200

    Merging

--- server.go
+++ server.go
@@ -1,23 +1,23 @@
 package main
 
 import (
-	"github.com/xlab-si/e2ee-server/routers"
-	"github.com/xlab-si/e2ee-server/core/db"
-	"github.com/xlab-si/e2ee-server/config"
 	"github.com/codegangsta/negroni"
 	"github.com/rs/cors"
+	"github.com/xlab-si/e2ee-server/config"
+	"github.com/xlab-si/e2ee-server/core/db"
+	"github.com/xlab-si/e2ee-server/routers"
 	"net/http"
 )
 
 func main() {
 	c := cors.New(cors.Options{
-                AllowedOrigins:   []string{"*"},
-                AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE"},
-                AllowCredentials: true,
+		AllowedOrigins:   []string{"*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE"},
+		AllowCredentials: true,
 		//AllowedHeaders: []string{"Authorization"}, // doesn't work
-		AllowedHeaders:   []string{"*"},
+		AllowedHeaders: []string{"*"},
 		//Debug: true,
-        })
+	})
 
 	config.Init()
 	db.Init()
@@ -25,5 +25,6 @@ func main() {
 	n := negroni.Classic()
 	n.Use(c)
 	n.UseHandler(router)
+	
 	http.ListenAndServe(":8080", n)
 }
